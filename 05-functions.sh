87cb5b254eed2888f95b12c4cb06e68d6c6a2907
YOGESH A C <yogeshacy@outlook.com>
2023-10-29 13:08:29 +0530

    updating ./learning-shell

--- 05-functions.sh
+++ 05-functions.sh
@@ -2,8 +2,14 @@
 
 ## Declare a function
 Sample() {
+  a=30
   echo sample function
+  echo a from main = $a
+  b=20
 }
 
 ## Access the function, simply call the function
+a=10
 Sample
+echo b from the function = $b
+echo a in main = $a
