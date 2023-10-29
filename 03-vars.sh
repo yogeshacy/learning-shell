32467ce19e96c74f773614ad55e06c8d0fe1c43b
YOGESH A C <yogeshacy@outlook.com>
2023-10-29 12:02:53 +0530

    updating ./learning-shell

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# variable can have a-z, A-Z, 0-9, _(underscore)
+#special character not allowed
+#not start with number or _
 a=10
 
 echo Value of a = $a
@@ -9,3 +12,10 @@ echo Today date is $date
 
 date1=$(date %F)
 echo Today date is $date1
+
+b=$((2+3-5*6))
+echo VAlue of b is $b
+
+#environmnet variable
+echo Value of c = $c
+
