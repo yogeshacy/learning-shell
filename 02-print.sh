1127c2bbec7410ccb02a0ff97296ac6e48142130
YOGESH A C <yogeshacy@outlook.com>
2023-10-29 09:56:38 +0530

    updating ./learning-shell

--- 02-print.sh
+++ 02-print.sh
@@ -22,7 +22,7 @@ echo -e "Hello\t\tWorld"
 #MAGENTA  35
 #CYAN     36
 
-echo -e "\tColor\t\tCode"
+echo -e "Color\tCode"
 echo -e "\e[31mRED      31\e[0m"
 echo -e "\e[32mGREEN    32\e[0m"
 echo -e "\e[33mYELLOW   33\e[0m"
